2cb862e99733794b82000b59a9af0fe95c618412
songiwon <biob1121@naver.com>
2022-06-23 00:26:23 +0900

    :feat SessionThree 추가

---
+++ danguback/src/components/Sessions/SessionThree.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./styled";
+
+const SessionThree = ()=>{
+    return(
+        <S.SessionContainer>
+            <S.SeThreeTitle>프로젝트 진행중 입니다</S.SeThreeTitle>
+            <S.SeThreeText>COMMING SOON...</S.SeThreeText>
+        </S.SessionContainer>
+    )
+}
+
+export default SessionThree
