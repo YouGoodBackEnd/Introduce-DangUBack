5a448449a26671ab3c0c8b109310fda793286ad9
songiwon <biob1121@naver.com>
2022-06-23 00:26:42 +0900

    :feat SessionFour 추가

---
+++ danguback/src/components/Sessions/SessionFour.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import * as S from "./styled";
+import ArrowLow from "../../assets/svg/ArrowLow.svg";
+import ArrowHigh from "../../assets/svg/ArrowHigh.svg";
+
+const SessionFour = () => {
+  const [members, setMembers] = useState<any>([
+    { Field: "FrontEnd", Name: ["손지원"], active: false, part: "당" },
+    { Field: "Android", Name: ["이용진"], active: false, part: "유" },
+    { Field: "Designer", Name: ["손지원"], active: false, part: "백" },
+  ]);
+
+  const membersMap = members.map((x: any, index: number) => {
+    return (
+      <S.FieldBox key={index}>
+        <S.FieldName>{x.Field}</S.FieldName>
+        <S.FieldImg src={x.active ? ArrowHigh : ArrowLow} className={x.Field} />
+        {x.active ? (
+          <S.MembersBoxOn>
+            {x.Name.map((x: string) => {
+              return <S.MembersName>{x}</S.MembersName>;
+            })}
+          </S.MembersBoxOn>
+        ) : (
+          <S.MembersBoxOff>{x.part}</S.MembersBoxOff>
+        )}
+      </S.FieldBox>
+    );
+  });
+
+  return (
+    <S.SessionContainer>
+      <S.SeTwoTitle>Members</S.SeTwoTitle>
+      <S.SeFourLine />
+      <S.FieldLayout>{membersMap}</S.FieldLayout>
+    </S.SessionContainer>
+  );
+};
+
+export default SessionFour;
